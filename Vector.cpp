295a05bf009d3fceebee1d61a157cb70f79f6241
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2023-06-24 23:17:57 +0600

    vector

--- Vector.cpp
+++ Vector.cpp
@@ -18,15 +18,19 @@ int main()
     p = make_pair(2, "abc");
     cout << p.first << " " << p.second << endl;
 
-    vector<int> v;
+    vector<int> v(5, 3);
     int n;
     cin >> n;
     for(int i = 0; i < n; i++)
     {
         int x;
         cin >> x;
-        v.push_back(x);
+        v.push_back(x);   // O(1)
     }
+
+    vector<int> v2 = v;  // O(n)
+    v2.push_back(10);
     printVec(v);
+    printVec(v2);
      
 }
