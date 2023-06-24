b5350ff7490008a88f1520269fe371b0a46d0c23
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2023-06-24 22:48:15 +0600

    vector

--- Vector.cpp
+++ Vector.cpp
@@ -3,9 +3,30 @@
 #include<vector>
 using namespace std;
 
+void printVec(vector<int> v)
+{
+    cout << "size = " << v.size() << endl;
+    for(int i = 0; i < v.size(); ++i)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
 int main()
 {
     pair<int, string> p;
     p = make_pair(2, "abc");
     cout << p.first << " " << p.second << endl;
+
+    vector<int> v;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    printVec(v);
+     
 }
