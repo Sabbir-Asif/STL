507db1af5a7ec7c61118037c0dc3dce432d069d9
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2023-06-24 22:41:08 +0600

    pair

---
+++ Vector.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<utility>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    pair<int, string> p;
+    p = make_pair(2, "abc");
+    cout << p.first << " " << p.second << endl;
+}
