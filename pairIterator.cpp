9aa50a94ec0097e58f0d04d6c9dcd9f32e67c134
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2023-06-29 19:44:53 +0600

    iterator of pair

---
+++ pairIterator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+#include<utility>
+using namespace std;
+
+int main()
+{
+    vector<pair<int, int> > vp;
+
+    for(int i = 0; i < 5; i++)
+    {
+        vp.push_back(make_pair(i, i+1));
+    }
+    
+    vector<pair<int, int> > :: iterator it;
+
+    for(it = vp.begin(); it != vp.end(); it++)
+    {
+        cout << (*it).first << " " << (*it).second << endl;
+    }
+}
